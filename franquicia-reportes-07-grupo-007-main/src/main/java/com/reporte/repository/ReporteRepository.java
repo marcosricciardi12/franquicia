fafe52c7b1bc6379364e1b0f5621951733a84e20
marcosricciardi12 <marcosricciardi12@gmail.com>
2023-06-21 17:42:24 -0300

    remove data

--- franquicia-reportes-07-grupo-007-main/src/main/java/com/reporte/repository/ReporteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.reporte.repository;
-
-import com.reporte.models.Reporte;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReporteRepository extends JpaRepository<Reporte, Long> {
-}
